a6395a699cef335cba050a237f70a8274a9eca7b
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2024-04-05 14:56:13 +0200

    Made constructor for Request class

---
+++ Genspil Team 6/Request.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genspil_Team_6
+{
+    internal class Request(string gameName, int requestID, string customerName, string customerPhone)
+    {
+        public string GameName { get; set; } = gameName;
+        public int RequestID { get; set; } = requestID;
+        public string CustomerName { get; set; } = customerName;
+        public string CustomerPhone { get; set; } = customerPhone;
+    }
+}
