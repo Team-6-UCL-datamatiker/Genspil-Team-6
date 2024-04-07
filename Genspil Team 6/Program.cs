fc90793fb5e6f4ea306857b1f555fc9bd4b5edb4
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2024-04-07 12:14:27 +0200

    Display menu methods added

--- Genspil Team 6/Program.cs
+++ Genspil Team 6/Program.cs
@@ -11,6 +11,38 @@ namespace Genspil_Team_6
 
         }
 
+        //Display methods
+        static void DisplayMainMenu()
+        {
+            Console.WriteLine("Welcome to the Genspil Database System!\n");
+            Console.WriteLine("------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("1. Display Inventory");
+            Console.WriteLine("2. Search Games");
+            Console.WriteLine("3. Add Game");
+            Console.WriteLine("4. Remove Game");
+            Console.ResetColor();
+            Console.WriteLine("------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("5. Display Requests");
+            Console.WriteLine("6. Add Request");
+            Console.WriteLine("7. Remove Request");
+            Console.ResetColor();
+            Console.WriteLine("------------------------");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("8. Exit");
+            Console.ResetColor();
+            Console.WriteLine("------------------------");
+            Console.Write("Enter your choice: ");
+        }
+        static void DisplaySearchMenu()
+        {
+            Console.WriteLine("Filter Options:\n");
+            Console.WriteLine("1. Add Filter");
+            Console.WriteLine("2. Clear Filters");
+            Console.WriteLine("3. Return to Main Menu");
+            Console.Write("Enter your choice: ");
+        }
         static void DisplayLogo()
         {
             // Logo
