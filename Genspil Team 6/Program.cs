7fbcac6b1a3edb6503ea2c0a4316707a64c361e7
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2024-04-07 12:13:31 +0200

    DisplayLogo method added

--- Genspil Team 6/Program.cs
+++ Genspil Team 6/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Genspil_Team_6
+﻿using Genspil_Team_6;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Genspil_Team_6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+
         }
+
+        static void DisplayLogo()
+        {
+            // Logo
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("  ________                                 .__ .__   \r\n /  _____/   ____    ____    ____________  |__||  |  \r\n/   \\  ___ _/ __ \\  /    \\  /  ___/\\____ \\ |  ||  |  \r\n\\    \\_\\  \\\\  ___/ |   |  \\ \\___ \\ |  |_> >|  ||  |__\r\n \\______  / \\___  >|___|  //____  >|   __/ |__||____/\r\n        \\/      \\/      \\/      \\/ |__|              \n");
+            Console.ResetColor();
+        }
+
     }
 }
