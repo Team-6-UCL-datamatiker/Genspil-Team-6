cb7e238023e29aada35568fdfba9aaab3f71dba8
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2024-04-08 11:56:43 +0200

    SaveUsersToFile method added

--- Genspil Team 6/UserManager.cs
+++ Genspil Team 6/UserManager.cs
@@ -9,6 +9,7 @@ namespace Genspil_Team_6
     internal class UserManager
     {
         private List<Employee> employees = new List<Employee>();
+        private string userDatabasePath = @"C:\Users\Jeppe Lynge\Documents\Datamatiker\Projekt 3 - Genspil\Genspil-Team-6\Genspil Team 6\UserDatabase.txt";
 
         public UserManager()
         {
@@ -43,14 +44,27 @@ namespace Genspil_Team_6
             string username = Console.ReadLine();
 
             employees.Add(new Employee(username, "1", 1));
+            SaveUsersToFile();
             Console.WriteLine($"{username} added to the system!");
             Console.ReadLine();
         }
 
         public void SaveUsersToFile()
         {
+            List<string> lines = new List<string>();
 
+           foreach(Employee employee in employees)
+            {
+                lines.Add(employee.Name);
+            }
+            File.WriteAllLines(userDatabasePath, lines);
         }
+
+        //public void LoadUsersToFile()
+        //{
+        //    this.employees.Clear();
+
+        //}
     }
 
 
