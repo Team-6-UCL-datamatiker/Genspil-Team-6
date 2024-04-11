dbd2e3eaf5549e6dc94872e6600137642869f091
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2024-04-11 13:12:59 +0200

    Merged

--- Genspil Team 6/Request.cs
+++ Genspil Team 6/Request.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Genspil_Team_6
 {
-    internal class Request(string gameName, int requestID, string customerName, int customerPhoneNumber)
+    public class Request(string gameName, int requestID, string customerName, string customerPhone)
     {
         public string GameName { get; set; } = gameName;
         public int RequestID { get; set; } = requestID;
         public string CustomerName { get; set; } = customerName;
-        public int CustomerPhoneNumber { get; set; } = customerPhoneNumber;
+        public string CustomerPhone { get; set; } = customerPhone;
     }
 }
