08eb9e09e9fba11b9daff1a0ae1bf9d6043f0061
Jeppe Lynge Hansen <jklh62358@edu.ucl.dk>
2024-04-11 13:11:36 +0200

    Merged

--- Genspil Team 6/Game.cs
+++ Genspil Team 6/Game.cs
@@ -6,14 +6,21 @@ using System.Threading.Tasks;
 
 namespace Genspil_Team_6
 {
-    internal class Game(string name, string genre, string condition, int gameID, int noOfPlayers, double price, bool available)
+    public class Game(string gameName, string genre, string condition, int gameID, int noOfPlayers, double price, bool available)
     {
-        public string Name { get; set; } = name;
+        public string GameName { get; set; } = gameName;
         public string Genre { get; set; } = genre;
         public string Condition { get; set; } = condition;
         public int GameID { get; set; } = gameID;
         public int NoOfPlayers { get; set; } = noOfPlayers;
         public double Price { get; set; } = price;
         public bool Available { get; set; } = available;
+
+        public override string ToString()
+        {
+            return $"ID: #{GameID}\nGame: {GameName}\nPrice: {Price}\nGenre: {Genre}\nNumber of Players: {NoOfPlayers}\nCondition: {Condition}\nAvailable: {(Available ? "Yes" : "No")}";
+        }
     }
+
+
 }
