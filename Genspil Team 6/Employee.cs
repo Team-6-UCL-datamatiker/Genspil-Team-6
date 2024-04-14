71237d4b99d3e09eacbfcb48f84ee06e015b105e
Jesper Helstrup <jehe63306@edu.ucl.dk>
2024-04-14 11:28:01 +0200

    removed useless text

--- Genspil Team 6/Employee.cs
+++ Genspil Team 6/Employee.cs
@@ -6,12 +6,10 @@ using System.Threading.Tasks;
 
 namespace Genspil_Team_6
 {
-    public class Employee(string name, string password, int accessLevel, int testc)
+    public class Employee(string name, string password, int accessLevel)
     {
         public string Name { get; set; } = name;
         public string Password { get; set; } = password;
         public int AccessLevel { get; set; } = accessLevel;
-
-        public int test { get; set; } = testc;
     }
 }
